9fd86a51c4b08eed173c7ae76e09a3e8df417779
wpalchemist <morgan@wpalchemists.com>
2018-07-16 09:41:10 -0700

    remove page title from homepage

--- panel-page.php
+++ panel-page.php
@@ -10,13 +10,41 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-            <?php
-            while ( have_posts() ) : the_post();
+            <?php while ( have_posts() ) : the_post(); ?>
 
-                get_template_part( 'template-parts/content', 'panel-page' );
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); shoreditch_background_image(); ?>>
+                    <div class="hentry-wrapper">
 
-            endwhile; // End of the loop.
-            ?>
+                        <div class="entry-content">
+                            <?php
+                            the_content();
+
+                            wp_link_pages( array(
+                                'before'      => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'shoreditch' ) . '</span>',
+                                'after'       => '</div>',
+                                'link_before' => '<span>',
+                                'link_after'  => '</span>',
+                                'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'shoreditch' ) . ' </span>%',
+                                'separator'   => '<span class="screen-reader-text">, </span>',
+                            ) );
+                            ?>
+                        </div><!-- .entry-content -->
+
+                        <?php
+                        edit_post_link(
+                            sprintf(
+                            /* translators: %s: Name of current post */
+                                esc_html__( 'Edit %s', 'shoreditch' ),
+                                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                            ),
+                            '<footer class="entry-footer"><span class="edit-link">',
+                            '</span></footer><!-- .entry-footer -->'
+                        );
+                        ?>
+                    </div><!-- .hentry-wrapper -->
+                </article><!-- #post-## -->
+
+            <?php endwhile; // End of the loop. ?>
 
             <div id="join-conversation" class="has-cerulean-background-color text-center">
                 <h4>
